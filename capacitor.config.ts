a566af2e7ce7a920ee63a8448f09b09c4e869113
ARAOV <araoolmoos@hotmail.com>
2021-12-02 03:01:53 -0600

    cita (alinear)

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hackathon_covadonga_patient',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
