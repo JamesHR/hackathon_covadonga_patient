fc8bf93135f74e89de1c4b8d1122ebb5e420c288
Lola Glez <mariad.glez0@gmail.com>
2021-12-02 01:37:45 -0600

    Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,12 +12,21 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'login',
+    path: 'loginara',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'cita',
     loadChildren: () => import('./cita/cita.module').then( m => m.CitaPageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
+  },
+
 
 
 ];
